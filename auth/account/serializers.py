dccb71355192501542ba4f4303b0b4b21b4bfc64
Pushar Singhal <pusinghal010@gmail.com>
2025-10-10 12:46:42 +0530

    added serialiser and view code for login

--- auth/account/serializers.py
+++ auth/account/serializers.py
@@ -19,3 +19,22 @@ class UserRegistrationSerializer(serializers.ModelSerializer):
 
     def create(self,validate_data):
         return User.object.create_user(**validate_data)
+    
+class UserLoginSerializer(serializers.ModelSerializer):
+    email=serializers.EmailField(max_length=255)
+    class Meta:
+        model=User
+        fields=["email","password"]
+        extra_kwargs={
+            'password':{'write_only':True}
+        }
+    
+    def validate(self,data):
+        password=data.get('password')
+        password2=data.get('password2')
+        if password != password2:
+            raise serializers.ValidationError("Password and confirm password doesn't match")
+        return data
+
+    def create(self,validate_data):
+        return User.object.create_user(**validate_data)
