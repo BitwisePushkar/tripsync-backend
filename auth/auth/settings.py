069cff181e6b1b78f560fbfacdab32fab652ded4
Arnavonawhim <agrawal.arnav20@gmail.com>
2025-10-13 22:29:54 +0530

    fixed settings

--- auth/auth/settings.py
+++ auth/auth/settings.py
@@ -165,8 +165,7 @@ if not DEBUG:
     SECURE_CONTENT_TYPE_NOSNIFF = True
     X_FRAME_OPTIONS = 'DENY'
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
