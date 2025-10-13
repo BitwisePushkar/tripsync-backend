80c330fd80247584dcede3406af38662488dc7b0
Arnavonawhim <agrawal.arnav20@gmail.com>
2025-10-14 05:05:09 +0530

    switched to sendgrid for email

--- auth/auth/settings.py
+++ auth/auth/settings.py
@@ -205,17 +205,15 @@ else:
     CSRF_COOKIE_SECURE = False
     SESSION_COOKIE_SECURE = False
 
-# Email Configuration
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend' 
-EMAIL_HOST = config('EMAIL_HOST', default='smtp.gmail.com')
+EMAIL_HOST = config('EMAIL_HOST', default='smtp.sendgrid.net')
 EMAIL_PORT = config('EMAIL_PORT', default=587, cast=int)
 EMAIL_USE_TLS = config('EMAIL_USE_TLS', default=True, cast=bool)
-EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='')
-EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='')
+EMAIL_HOST_PASSWORD = config('SENDGRID_API_KEY')
+EMAIL_HOST_USER = 'api key'
 DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL', default=f'TripSync <{EMAIL_HOST_USER}>')
 EMAIL_TIMEOUT = 30 
 
-# Admin Settings
 ADMIN_SITE_HEADER = "TripSync Administration"
 ADMIN_SITE_TITLE = "TripSync Admin"
 ADMIN_INDEX_TITLE = "Welcome to TripSync Admin Portal"
