d9e4a2d6dc3a3ba88fae727e810707e1569be51b
Pushar Singhal <pusinghal010@gmail.com>
2025-10-13 21:08:36 +0530

    updated setting.py

--- auth/auth/settings.py
+++ auth/auth/settings.py
@@ -70,7 +70,9 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-DATABASES['default'] = dj_database_url.config(default=os.environ.get("DATABASE_URL"))
+database_url = os.environ.get("DATABASE_URL")
+if database_url:
+    DATABASES['default'] = dj_database_url.parse(database_url)
 
 
 REST_FRAMEWORK = {
