a582210b0a8609956850bf954bddcc35b7a3ae7a
Arnavonawhim <agrawal.arnav20@gmail.com>
2025-10-16 12:28:34 +0530

    Add database reset command and build script

--- auth/build.sh
+++ auth/build.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
-   set -o errexit
-   
-   pip install --upgrade pip
-   pip install -r requirements.txt
-   
-   python manage.py collectstatic --no-input
-   python manage.py migrate
+set -o errexit
+
+pip install --upgrade pip
+pip install -r requirements.txt
+
+python manage.py collectstatic --no-input
+
+python manage.py migrate --run-syncdb
+echo "Build completed successfully!"
