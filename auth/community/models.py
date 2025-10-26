9cc21af5a37de4ea40c6244e70a2f4ec78138445
Arnavonawhim <agrawal.arnav20@gmail.com>
2025-10-26 17:56:45 +0530

    added cloudinary for image and videos upload to show to all users and connected to db

--- auth/community/models.py
+++ auth/community/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    profilename=models.CharField(max_legnth=50)
+    title = models.CharField(max_legnth=50)
+    desc = models.CharField(max_length=1000)
+    loc = models.charField(max_legnth=75,blank=True,null=True)
+    loc_rating=models.IntegerField(blank=True,null=True)
+    img = models.ImageField(upload_to='images/', blank=True, null=True)
+    vid = models.FileField(upload_to='videos/', blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+    
