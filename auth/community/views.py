f2cbccb0b0ceb1cfdcf6eb665356c837de41718f
Arnavonawhim <agrawal.arnav20@gmail.com>
2025-10-27 02:35:43 +0530

    added aws s3 instead of cloudinary

--- auth/community/views.py
+++ auth/community/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework import serializers
+from .models import Post
 
-# Create your views here.
+class PostSerializer(serializers.ModelSerializer):
+    user = serializers.StringRelatedField(read_only=True)
+    img_url = serializers.SerializerMethodField()
+    vid_url = serializers.SerializerMethodField()
+
+    
