c8f2ded2a4b98462ba4f3cb13e9d7e040780a03d
Pushkar Singhal <psinghal0135@gmail.com>
2025-10-28 15:01:25 +0530

    made changes in code acc to design

--- auth/auth/urls.py
+++ auth/auth/urls.py
@@ -23,7 +23,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/account/', include('account.urls')),
     path('api/chat/', include('chat.urls')),
-    path('api/profile/', include('personal.urls')), 
+    path('api/personal/', include('personal.urls')), 
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('api/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
