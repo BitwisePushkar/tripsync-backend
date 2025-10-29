6cfc51004ff2fc3e5149bdce79b9e3d0d46b04e1
Arnavonawhim <agrawal.arnav20@gmail.com>
2025-10-29 06:49:54 +0530

    restarting Itenary with models and serializers according to design

--- auth/ItenaryMaker/models.py
+++ auth/ItenaryMaker/models.py
@@ -1,4 +1,15 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
-class ChatSession (models.Model):
-    id
+class ItenaryFields(models.Model):
+    tripname = models.CharField(max_length=100)
+    current_loc = models.CharField(max_legnth=200)
+    destination = models.CharField(max_length=50)
+    trending = models.CharField()
+    start_date = models.DateField()
+    end_date = models.DateField()
+    days = models.IntegerField(blank=True,null=True,validators=[MinValueValidator(1),MaxValueValidator(7)])
+    trip_type = models.CharField()
+    trip_preferences = models.CharField()
+    Budget = models.FloatField()    
+
---
+++ auth/ItenaryMaker/serializers.py
@@ -0,0 +1,10 @@
+from rest_framework import serializers
+from .models import ItenaryFields
+
+class ItenarySerializer(serializers.ModelSerializer):
+
+
+    class Meta:
+        model = ItenaryFields
+        fields = ['tripname','current_loc','destination','trending','start_date','end_date','days','trip_type','trip_preferences','Budget']
+        read_only_fields = ['tripname','destination','trending']
