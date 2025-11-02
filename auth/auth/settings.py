0ffe96cbaf4ee408ec58c7fa89e2ee8fd8a1a20f
Pushkar Singhal <psinghal0135@gmail.com>
2025-11-02 19:13:12 +0530

    did some bug fix in code

--- auth/auth/settings.py
+++ auth/auth/settings.py
@@ -33,6 +33,8 @@ INSTALLED_APPS = ['channels',
                   'personal.apps.PersonalConfig',
                   'HomePage.apps.HomepageConfig',
                   'expense.apps.ExpenseConfig',
+                  'Itinerary.apps.ItineraryConfig',
+                #   'tripmate.apps.TripmateConfig',
                 ]
 
 MIDDLEWARE = ['django.middleware.security.SecurityMiddleware', 'whitenoise.middleware.WhiteNoiseMiddleware', 'django.contrib.sessions.middleware.SessionMiddleware', 'corsheaders.middleware.CorsMiddleware', 'django.middleware.common.CommonMiddleware', 'django.middleware.csrf.CsrfViewMiddleware', 'django.contrib.auth.middleware.AuthenticationMiddleware', 'django.contrib.messages.middleware.MessageMiddleware', 'django.middleware.clickjacking.XFrameOptionsMiddleware']
