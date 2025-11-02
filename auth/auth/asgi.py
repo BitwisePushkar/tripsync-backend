2445b363a0b322d0a0afc3de52cac290ec2f096b
Pushkar Singhal <psinghal0135@gmail.com>
2025-11-02 20:20:39 +0530

    add line for deployment

--- auth/auth/asgi.py
+++ auth/auth/asgi.py
@@ -1,9 +1,12 @@
 import os
+import django
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'auth.settings')
+
+django.setup()
 
 import tripmate.routing
 import chat.routing
