4eef240a4439419b544d1a6a7af9d14f31f0de5c
Pushkar Singhal <psinghal0135@gmail.com>
2025-11-05 17:19:02 +0530

    did some bug fixes

--- auth/auth/asgi.py
+++ auth/auth/asgi.py
@@ -1,16 +1,20 @@
 import os
+import django
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'auth.settings')
 
-# import tripmate.routing
+django.setup()
+
+import tripmate.routing
 import chat.routing
 
 application = ProtocolTypeRouter({
     'http': get_asgi_application(),
     'websocket':AuthMiddlewareStack(
-        URLRouter(chat.routing.websocket_urlpatterns)
+        URLRouter(tripmate.routing.websocket_urlpatterns +
+            chat.routing.websocket_urlpatterns)
     ),
 })
