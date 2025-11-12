955080e6070b233657ad3ab390621ef52c5ba018
Pushkar Singhal <psinghal0135@gmail.com>
2025-11-12 14:56:53 +0530

    solving websocket issue

--- auth/auth/asgi.py
+++ auth/auth/asgi.py
@@ -1,18 +1,21 @@
 import os
 import django
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'auth.settings')
+django.setup()
+
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
+from channels.security.websocket import AllowedHostsOriginValidator
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'auth.settings')
-
-django.setup()
-
-import chat.routing
+from chat.routing import websocket_urlpatterns
 
 application = ProtocolTypeRouter({
     'http': get_asgi_application(),
-    'websocket':AuthMiddlewareStack(
-        URLRouter(chat.routing.websocket_urlpatterns)
+    'websocket':AllowedHostsOriginValidator(
+        AuthMiddlewareStack(
+            URLRouter(websocket_urlpatterns)
+        )
     ),
 })
--- auth/auth/settings.py
+++ auth/auth/settings.py
@@ -99,7 +99,7 @@ CORS_ALLOWED_ORIGINS = config('CORS_ALLOWED_ORIGINS', default='http://localhost:
 CORS_ALLOW_ALL_ORIGINS = DEBUG  
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOW_METHODS = ['DELETE', 'GET', 'OPTIONS', 'PATCH', 'POST', 'PUT']
-CORS_ALLOW_HEADERS = ['accept', 'accept-encoding', 'authorization', 'content-type', 'dnt', 'origin', 'user-agent', 'x-csrftoken', 'x-requested-with']
+CORS_ALLOW_HEADERS = ['accept', 'accept-encoding', 'authorization', 'content-type', 'dnt', 'origin', 'user-agent', 'x-csrftoken', 'x-requested-with','sec-websocket-protocol','sec-websocket-extensions', ]
 
 if not DEBUG:
     SECURE_SSL_REDIRECT = False
