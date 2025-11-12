1fc33103d2389cd955e40c717f1039de2be412ac
Pushkar Singhal <psinghal0135@gmail.com>
2025-11-12 21:25:37 +0530

    solving websocket issue

--- auth/auth/asgi.py
+++ auth/auth/asgi.py
@@ -2,17 +2,20 @@ import os
 import django
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'auth.settings')
-
 django.setup()
 
-<<<<<<< HEAD
-import chat.routing
+from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+from channels.security.websocket import AllowedHostsOriginValidator
+
+from chat.routing import websocket_urlpatterns
 
 application = ProtocolTypeRouter({
     'http': get_asgi_application(),
-    'websocket':AuthMiddlewareStack(
-        URLRouter(chat.routing.websocket_urlpatterns)
+    'websocket':AllowedHostsOriginValidator(
+        AuthMiddlewareStack(
+            URLRouter(websocket_urlpatterns)
+        )
     ),
 })
-=======
->>>>>>> main
